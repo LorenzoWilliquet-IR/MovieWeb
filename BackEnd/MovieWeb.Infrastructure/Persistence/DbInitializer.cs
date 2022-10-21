8280007b1ca06d5332f3d78d9d18660a113825bf
RobbeKR <robbekerkaert@gmail.com>
2022-10-21 15:42:13 +0200

    added some movies

--- BackEnd/MovieWeb.Infrastructure/Persistence/DbInitializer.cs
+++ BackEnd/MovieWeb.Infrastructure/Persistence/DbInitializer.cs
@@ -19,7 +19,23 @@ namespace MovieWeb.Infrastructure.Persistence
                 new Movie()
                 {
                     Name = "Jaws", DurationInMinutes = 120, ReleaseDate = new DateTime(1999,12,10)
-                }
+                },
+                new Movie()
+                {
+                    Name = "The Lord of The Rings: Fellowship of the Ring", DurationInMinutes = 178, ReleaseDate = new DateTime(2001,12,19)
+                },
+                new Movie()
+                {
+                    Name = "Scream", DurationInMinutes = 133, ReleaseDate = new DateTime(2005,05,05)
+                },
+                new Movie()
+                {
+                    Name = "The Godfather", DurationInMinutes = 175, ReleaseDate = new DateTime(1973,01,18)
+                },
+                new Movie()
+                {
+                    Name = "The Incredibles", DurationInMinutes = 90, ReleaseDate = new DateTime(2010,01,18)
+                },
             };
 
             foreach (var movie in movies)
@@ -38,6 +54,10 @@ namespace MovieWeb.Infrastructure.Persistence
                 new Actor()
                 {
                     FirstName = "Johnny", LastName = "Depp", Birthdate =  new DateTime(1963,6,9), MovieId = 1
+                },
+                new Actor()
+                {
+                    FirstName = "Elijah", LastName = "Wood", Birthdate =  new DateTime(1963,6,9), MovieId = 2
                 }
             };
 
