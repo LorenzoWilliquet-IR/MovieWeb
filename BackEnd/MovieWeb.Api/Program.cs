7c3d4531d3422463bbb5d97bc51ea43fd973d504
RobbeKR <robbekerkaert@gmail.com>
2022-10-21 10:31:27 +0200

    added automapper and base controller

--- BackEnd/MovieWeb.Api/Program.cs
+++ BackEnd/MovieWeb.Api/Program.cs
@@ -1,29 +1,27 @@
 using MediatR;
-using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using MovieWeb.Application.Common.Interfaces;
 using MovieWeb.Application.Common.Movies;
 using MovieWeb.Infrastructure.Persistence;
 using System.Reflection;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddTransient<IMovieService, MovieService>();
+
+builder.Services.AddDbContext<MovieDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers().AddJsonOptions(x =>
+                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-var connection = new SqliteConnection("DataSource=:memory:");
-connection.Open();
-
-builder.Services.AddDbContext<MovieDbContext>(options =>
-{
-    options.UseSqlite(connection);
-});
-
-builder.Services.AddScoped<IMovieService, MovieService>();
 
 builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
 
@@ -32,8 +30,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MovieWeb v1"));
 }
 
 app.UseHttpsRedirection();
